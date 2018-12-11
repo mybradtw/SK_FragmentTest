2e7bc08e6dfcb92b03f62d88810b82bb8036dab5
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2018-12-11 16:31:24 +0800

    v2

--- app/src/main/java/tw/brad/sk_barcodefragmenttest/F1.java
+++ app/src/main/java/tw/brad/sk_barcodefragmenttest/F1.java
@@ -7,15 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class F1 extends Fragment {
+    private TextView txt;
+    private Button btn;
+    private View mainView;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_f1, container, false);
+        if (mainView == null) {
+            mainView = inflater.inflate(
+                    R.layout.fragment_f1, container, false);
+            txt = mainView.findViewById(R.id.f1_txt);
+            btn = mainView.findViewById(R.id.f1_btn);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showLottery();
+                }
+            });
+        }
+        return mainView;
     }
+
+    private void showLottery(){
+        int lottery = (int)(Math.random()*49+1);
+        txt.setText("" + lottery);
+    }
+
 
     @Override
     public void onAttach(Context context) {
--- app/src/main/java/tw/brad/sk_barcodefragmenttest/MainActivity.java
+++ app/src/main/java/tw/brad/sk_barcodefragmenttest/MainActivity.java
@@ -1,19 +1,44 @@
 package tw.brad.sk_barcodefragmenttest;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private F1 f1;
+    private F2 f2;
+    private FragmentManager fmgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        f1 = new F1(); f2 = new F2();
+        fmgr = getSupportFragmentManager();
+
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.add(R.id.container, f1);
+        transaction.commit();
+
     }
 
     public void gotoF1(View view) {
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.replace(R.id.container, f1);
+        transaction.commit();
     }
     public void gotoF2(View view) {
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.replace(R.id.container, f2);
+        transaction.commit();
+    }
+
+    public void gotoPage2(View view) {
+        Intent intent = new Intent(this, Page2Activity.class);
+        startActivity(intent);
     }
 }
