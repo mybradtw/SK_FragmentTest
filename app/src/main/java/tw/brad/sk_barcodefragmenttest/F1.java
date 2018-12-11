b3a62b2c19b27671b82a57a2ce75567c157b6a35
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2018-12-11 15:51:40 +0800

    v1

---
+++ app/src/main/java/tw/brad/sk_barcodefragmenttest/F1.java
@@ -0,0 +1,30 @@
+package tw.brad.sk_barcodefragmenttest;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class F1 extends Fragment {
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_f1, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
---
+++ app/src/main/java/tw/brad/sk_barcodefragmenttest/MainActivity.java
@@ -0,0 +1,19 @@
+package tw.brad.sk_barcodefragmenttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoF1(View view) {
+    }
+    public void gotoF2(View view) {
+    }
+}
